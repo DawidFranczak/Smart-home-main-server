2b1fb064957e8cd9915ccd5147c4db9b3f7d0826
DawidFranczak <daw.franczak@gmail.com>
2024-08-24 11:16:33 +0200

    performance improvement

--- aquarium/apps.py
+++ aquarium/apps.py
@@ -6,13 +6,3 @@ from django.apps import AppConfig
 class AquariumConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "aquarium"
-
-    def ready(self) -> None:
-        if os.environ.get("RUN_MAIN") == "true":
-            from threading import Thread
-
-            from .thread import aquas_check
-
-            thread = Thread(target=aquas_check)
-            thread.daemon = True
-            thread.start()
